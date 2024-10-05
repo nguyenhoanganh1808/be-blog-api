23e7b9f136226d2e0519b74bbc9f83a9246f4321
nguyenhoanganh1808 <1808nguyenhoanganh2003@gmail.com>
2024-10-05 10:13:09 +0700

    Feature: delete category

--- controllers/category_controller.js
+++ controllers/category_controller.js
@@ -60,6 +60,8 @@ exports.updateCategoryById = [
   }),
 ];
 
-exports.deleteCategoryById = (req, res) => {
-  res.send("delete category");
-};
+exports.deleteCategoryById = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const deleteCategory = await prisma.category.delete({ where: { id: id } });
+  res.status(200).json(deleteCategory);
+});
